d4993b516c9022217c1e934143f1d3428d49c3a4
Ted Shaffer <ted@brightsign.biz>
2017-07-03 06:53:44 -0700

    initial work on slick. failing to load css files.

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import Slider from 'react-slick';//TODO remove
+import "slick-carousel/slick/slick.scss";
+import "slick-carousel/slick/slick-theme.scss";
 
 import path = require('path');
 
@@ -28,26 +31,46 @@ export default class Image extends React.Component<ImageProps, object> {
     return true;
   }
 
+  // render() {
+  //
+  //   const self: Image = this;
+  //
+  //   if (this.timeout) {
+  //     debugger;
+  //   }
+  //
+  //   this.timeout = setTimeout( () => {
+  //       this.timeout = null;
+  //       self.props.onTimeout();
+  //     }
+  //     , this.props.duration);
+  //
+  //   return (
+  //     <img
+  //       src={this.props.src}
+  //       width={this.props.width.toString()}
+  //       height={this.props.height.toString()}
+  //     />
+  //   );
+  // }
+
   render() {
-
-    const self: Image = this;
-
-    if (this.timeout) {
-      debugger;
-    }
-
-    this.timeout = setTimeout( () => {
-        this.timeout = null;
-        self.props.onTimeout();
-      }
-      , this.props.duration);
-
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     return (
-      <img
-        src={this.props.src}
-        width={this.props.width.toString()}
-        height={this.props.height.toString()}
-      />
+      <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+// import webpack from 'webpack';
+// import ExtractTextPlugin from 'extract-text-webpack-plugin';
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractStyles = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  allChunks: true,
+  disable: true,
+});
+
 module.exports = {
   entry: "./src/index.tsx",
   output: {
@@ -20,7 +32,39 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.(sass|scss)$/,
+        loader: extractStyles.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              minimize: {
+                autoprefixer: {
+                  add: true,
+                  remove: true,
+                  browsers: ['last 2 versions'],
+                },
+                discardComments: {
+                  removeAll: true,
+                },
+                discardUnused: false,
+                mergeIdents: false,
+                reduceIdents: false,
+                safe: true,
+                sourcemap: true,
+              },
+            },
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }],
+        })
+      }
+    ],
   },
 };
