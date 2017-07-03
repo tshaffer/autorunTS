141e2ce90b702a20b5cc3a7c6a66b790bf4f73ee
Ted Shaffer <ted@brightsign.biz>
2017-07-03 07:38:30 -0700

    changes to make it look more reasonable.

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -55,20 +55,20 @@ export default class Image extends React.Component<ImageProps, object> {
   render() {
     var settings = {
       dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
+      // infinite: true,
+      // speed: 500,
+      slidesToShow: 2,
+      // slidesToScroll: 1
     };
     return (
-      <Slider {...settings}>
-        <div><h3>1</h3></div>
-        <div><h3>2</h3></div>
-        <div><h3>3</h3></div>
-        <div><h3>4</h3></div>
-        <div><h3>5</h3></div>
-        <div><h3>6</h3></div>
-      </Slider>
+      <div className='slickContainer'>
+        <Slider {...settings}>
+          <div><img src='http://placekitten.com/g/400/200' /></div>
+          <div><img src='http://placekitten.com/g/400/200' /></div>
+          <div><img src='http://placekitten.com/g/400/200' /></div>
+          <div><img src='http://placekitten.com/g/400/200' /></div>
+        </Slider>
+      </div>
     );
   }
 }
