ad0135c0c34eaa27ebf1841d032af0fd1916f647
Ted Shaffer <ted@brightsign.biz>
2017-08-01 14:43:23 -0700

    experiment - now commented out.

--- plugins/plugin-0.js
+++ plugins/plugin-0.js
@@ -4,3 +4,15 @@ exports.bar = 'bar';
 // exports.pizza = '<span>pizza</span>';
 var t = document.createTextNode("Pizza");       // Create a text node
 exports.text = t;
+
+// import * as React from 'react';
+//
+// class MyPizza extends React.Component {
+//   render() {
+//     return (
+//       <div>My Pizza</div>
+//     );
+//   }
+// }
+//
+// export default MyPizza;
